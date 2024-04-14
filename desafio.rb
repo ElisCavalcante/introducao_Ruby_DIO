b5f98253bd5c7976bc57208237840ceaa69533d3
ElisCavalcante <elisabeth.rsc@outlook.com>
2024-04-14 19:37:22 -0300

    Primeiro desafio de código em Ruby DIO

---
+++ desafio.rb
@@ -0,0 +1,10 @@
+print "Digite seu nome : "
+nome = gets.chomp
+print "Agora , digite seu sobrenome:  "
+sobrenome = gets.chomp
+print "Digite sua idade : "
+idade = gets.chomp.to_i
+
+
+
+print "Seu nome é #{nome} #{sobrenome} e sua idade é #{idade} "
